8cb5c368d4a14c68db578650a1da724877d178cd
Harsh1782 <bhisraharsh1782@gmail.com>
2024-02-02 20:28:46 +0530

    Module : sell API || create sell API and some changes

--- src/controller/tradeAction.ts
+++ src/controller/tradeAction.ts
@@ -13,12 +13,13 @@ import jwt from "jsonwebtoken";
 const jsondata = data;
 const funddata = fund;
 const ObjectId = mongoose.Types.ObjectId
-
+let usTime = new Date()
+let options = { timeZone: 'Asia/kolkata', hour12: false }
+let indiaTime = usTime.toLocaleString('en-US', options)
 export const buystock = async (req: Request, res: Response) => {
     try {
-        let buyAT = new Date()
-        let options = { timeZone: 'Asia/kolkata', hour12: false }
-        let indiaTime = buyAT.toLocaleString('en-US', options)
+
+
         console.log('indiaTime', indiaTime)
         const fundObj = funddata["data"];
         const body = req.body
@@ -52,12 +53,13 @@ export const buystock = async (req: Request, res: Response) => {
                 userTradeEnter.push({
                     trade_id: resultAdminTradeEnter._id,
                     user_id: id,
+                    tradingsymbol: body.tradingsymbol,
                     buyOrderId: "123456",
                     quantity: body.quantity,
                     isSelled: false,
                     buyAT: indiaTime,
                     accessToken: alluserdata[i].access_key,
-                    lessQuantity:false,
+                    lessQuantity: false,
                     buytradeStatus: null,
                 });
             } else if (Number(totalprice) > Number(fund)) {
@@ -68,12 +70,13 @@ export const buystock = async (req: Request, res: Response) => {
                     userTradeEnter.push({
                         trade_id: resultAdminTradeEnter._id,
                         user_id: id,
-                        order_id: "123456",
+                        tradingsymbol: body.tradingsymbol,
+                        buyOrderId: "123456",
                         quantity: quantity,
                         isSelled: false,
-                        buyAT: new Date(),
+                        buyAT: indiaTime,
                         accessToken: alluserdata[i].access_key,
-                        lessQuantity:true, 
+                        lessQuantity: true,
                         buytradeStatus: null,
                     });
                 } else if (quantity === 0) {
@@ -113,41 +116,53 @@ export const sellstock = async (req: Request, res: Response) => {
     try {
         const body = req.body
         const price = body.quantity * body.price;
+        const quantity = 12;
         // const totalprice = price.toFixed(11);
 
-        const adminTradeEnter: any = new adminTrade({
-            tradingsymbol: body.tradingsymbol,
-            exchange: body.exchange,
-            transaction_type: body.transaction_type,
-            order_type: body.order_type,
-            quantity: body.quantity,
-            product: body.product,
-            sellPrice: body.price,
-            sellAT: new Date()
-        })
+        // const adminTradeEnter: any = new adminTrade({
+        //     tradingsymbol: body.tradingsymbol,
+        //     exchange: body.exchange,
+        //     transaction_type: body.transaction_type,
+        //     order_type: body.order_type,
+        //     quantity: body.quantity,
+        //     product: body.product,
+        //     sellPrice: body.price,
+        //     sellAT: indiaTime
+        // })
 
-        const resultAdminTradeEnter = await adminTradeEnter.save();
+        // const resultAdminTradeEnter = await adminTradeEnter.save();
 
         const alluserdata = await userModel.find();
-        let userTradeEnter: any = [];
-        for (let i = 0; i < alluserdata.length; i++) {
-            const id = alluserdata[i]._id;
-            //here get user live margin data 
-            if (alluserdata[i].access_key) {
+        const tradeData = await userTrade.find();
 
-                userTradeEnter.push({
-                    trade_id: resultAdminTradeEnter._id,
-                    user_id: id,
-                    order_id: "123456",
-                    buyAT: new Date(),
-                    accessToken: alluserdata[i].access_key
-                });
+        if (tradeData) {
+            let counter = quantity;
+
+            for (let i = 0; i < alluserdata.length; i++) {
+                const id = alluserdata[i]._id;
+                if (counter !== 0) {
+
+                    for (let j = 0; j < tradeData.length; j++) {
+                        let data = tradeData[j].trade;
+                        for (let k = 0; k < data.length; k++) {
+                            console.log(`trade user id ${data[k].user_id} and actual id ${id}`);
+                            if (String(data[k].user_id) === String(id) && data[k].isSelled === false && data[k].quantity > 0 && counter !== 0) {
+
+                                counter = counter - data[k].quantity;
+                                const order_id = data[k].order_id;
+                                console.log("hello");
+                                const updatedata = await userTrade.findOneAndUpdate({ "trade.order_id": order_id }, { $set: { "trade.$.isSelled": true , "trade.$.sellAt" : indiaTime} })
+                            }
+                        }
+                    }
+                }
             }
+
+            // const insertdata = new userTrade({
+            //     trade: userTradeEnter
+            // })
+            // var resultUserTradeEnter = await insertdata.save();
         }
-        const insertdata = new userTrade({
-            trade: userTradeEnter
-        })
-        var resultUserTradeEnter = await insertdata.save();
 
         // kite.placeOrder(orderParams, (err, response) => {
         //     if (err) {
@@ -159,7 +174,7 @@ export const sellstock = async (req: Request, res: Response) => {
 
         // console.log(orderParams);
 
-        return res.status(200).json(new apiResponse(200, "sell stock details", { resultAdminTradeEnter, resultUserTradeEnter }, {}));
+        // return res.status(200).json(new apiResponse(200, "sell stock details", { resultAdminTradeEnter, resultUserTradeEnter }, {}));
     } catch (error) {
         return res.status(500).json(new apiResponse(500, responseMessage.internalServerError, {}, error));
     }
