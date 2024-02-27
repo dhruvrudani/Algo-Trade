707a2136813b7663e675a3c9c310c147aceef8bc
harsh1782 <bhisraharsh1782@gmail.com>
2024-02-27 20:10:14 +0530

    Module: create sub history API, changes in buy plan API and some changes

--- src/controller/user/planPurchase.ts
+++ src/controller/user/planPurchase.ts
@@ -11,6 +11,9 @@ import { Request, Response } from 'express'
 import jwt from "jsonwebtoken";
 import { checkPreferences } from "joi";
 import bcrypt from "bcryptjs";
+let buyAT = new Date();
+let options = { timeZone: 'Asia/Kolkata', hour12: false };
+let indiaTime = buyAT.toLocaleString('en-US', options);
 
 //buy plan
 /*
@@ -23,13 +26,37 @@ export const BuyPlan = async (req: Request, res: Response) => {
     try {
         let body = req.body;
 
-        const buyPlanData = await userModel.findByIdAndUpdate({ _id: body.id, role: 1 }, { plan: body.plan  }, { new: true });
+        const userData = await userModel.findByIdAndUpdate({ _id: body.id, role: 1 });
 
-        if (buyPlanData) {
-            res.status(200).json(new apiResponse(200, "plan buy successful", buyPlanData, {}))
-        } else {
-            res.status(401).json(new apiResponse(401, "some issue in plan buy", buyPlanData, {}))
+        if (userData.totalUsePlan === null) {
+            let buyPlanData;
+            if (userData.buyPlanAt === null) {
+
+                buyPlanData = await userModel.findByIdAndUpdate({ _id: body.id, role: 1 }, { totalUsePlan: 0, plan: body.plan, buyPlanAt: indiaTime }, { new: true });
+            } else if (userData.buyPlanAt !== null) {
+                buyPlanData = await userModel.findByIdAndUpdate({ _id: body.id, role: 1 }, { totalUsePlan: 0, plan: body.plan, updatePlanAt: indiaTime }, { new: true });
+            }
+            if (buyPlanData) {
+                res.status(200).json(new apiResponse(200, "plan buy successful", buyPlanData, {}))
+            } else {
+                res.status(401).json(new apiResponse(401, "some issue in plan buy", buyPlanData, {}))
+            }
+        } else if (userData.totalUsePlan !== null) {
+            let buyPlanData;
+            if (userData.buyPlanAt === null) {
+
+                buyPlanData = await userModel.findByIdAndUpdate({ _id: body.id, role: 1 }, { plan: body.plan, buyPlanAt: indiaTime }, { new: true });
+            } else if (userData.buyPlanAt !== null) {
+                buyPlanData = await userModel.findByIdAndUpdate({ _id: body.id, role: 1 }, { plan: body.plan, updatePlanAt: indiaTime }, { new: true });
+            }
+            if (buyPlanData) {
+                res.status(200).json(new apiResponse(200, "plan buy successful", buyPlanData, {}))
+            } else {
+                res.status(401).json(new apiResponse(401, "some issue in plan buy", buyPlanData, {}))
+            }
         }
+
+
     } catch (error) {
         return res.status(500).json(new apiResponse(500, responseMessage.internalServerError, {}, error))
     }
