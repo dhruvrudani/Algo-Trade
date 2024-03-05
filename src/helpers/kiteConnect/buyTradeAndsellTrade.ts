8916e15681e680a1dd5b3949685c5d9f2e122fd4
Harsh1782 <bhisraharsh1782@gmail.com>
2024-03-05 18:00:55 +0530

    changes in buy API and some changes

--- src/helpers/kiteConnect/buyTradeAndsellTrade.ts
+++ src/helpers/kiteConnect/buyTradeAndsellTrade.ts
@@ -4,7 +4,7 @@ import { adminTrade, userModel, userTrade, tradeQuantity } from "../../database"
 import { apiResponse } from "../../common";
 
 import fund from "../../helpers/funding.json";
-import { responseMessage, stockQuantity } from "../../helpers/index";
+import { buy, responseMessage, stockQuantity } from "../../helpers/index";
 import { getFundsAndMargins } from "../../helpers/kiteConnect/index";
 import { encryptData } from "../../common/encryptDecrypt";
 import mongoose from "mongoose";
@@ -29,132 +29,131 @@ export const buyTradeFunction = async (req: Request, res: Response, userData, bo
         // getFundsAndMargins(userData.access_key);
 
         let returnObj;
-        console.log(userData.access_key);
-        const fundObj = await getFundsAndMargins("7Fcmz5S9YRBXCnXUlrFnc6MGBb2W4KqU", 'equity').catch(error => {
+        const fundObj = await getFundsAndMargins(userData.access_key, 'equity').catch(error => {
             console.error("Error in getFundsAndMargins:", error);
             throw error;
         });
-        console.log(fundObj['quuity']);
-        // const { _id: id, access_key, isKiteLogin } = userData;
-        // if (quantityObj) {
-        //     const { quantity } = quantityObj;
-        //     const price = (quantity * body.price).toFixed(11);
-        //     const fund = Number(fundObj['equity'].net.toFixed(11));
-        //     if (access_key && isKiteLogin) {
+        console.log(fundObj);
+        const { _id: id, access_key, isKiteLogin } = userData;
+        if (quantityObj) {
+            const { quantity } = quantityObj;
+            const price = (quantity * body.price).toFixed(11);
+            const fund = Number(fundObj['equity'].net.toFixed(11));
+            if (access_key && isKiteLogin) {
 
-        //         if (Number(price) <= fund) {
-        //             const random5DigitNumber = generateRandomNumber();
-        //             const data: any = {
-        //                 access_key: userData.access_key,
-        //                 id: id,
-        //                 tradingsymbol: body.tradingsymbol,
-        //                 quantity: quantity,
-        //                 exchange: body.exchange,
-        //                 order_type: body.order_type,
-        //                 product: body.product
-        //             };
+                if (Number(price) <= fund) {
+                    const random5DigitNumber = generateRandomNumber();
+                    const data: any = {
+                        access_key: userData.access_key,
+                        id: id,
+                        tradingsymbol: body.tradingsymbol,
+                        quantity: quantity,
+                        exchange: body.exchange,
+                        order_type: body.order_type,
+                        product: body.product
+                    };
 
-        //             // buy(data);
-        //             const alluserdata = await userModel.findOneAndUpdate(
-        //                 {
-        //                     _id: userData._id,
-        //                     isActive: true,
-        //                     isDelete: false,
-        //                     isVerified: true
-        //                 },
-        //                 {
-        //                     $inc: {
-        //                         totalUsePlan: +1
-        //                     }
-        //                 },
-        //                 { new: true }
-        //             );
-        //             console.log(alluserdata);
-        //             return returnObj = {
-        //                 user_id: userData._id,
-        //                 tradingsymbol: body.tradingsymbol,
-        //                 buyOrderId: random5DigitNumber,
-        //                 quantity,
-        //                 isSelled: false,
-        //                 buyKitePrice: body.price,
-        //                 buyAT: indiaTime,
-        //                 accessToken: access_key,
-        //                 lessQuantity: false,
-        //                 buytradeStatus: null,
-        //             };
-        //         }
-        //         else {
-        //             const updatedQuantity = stockQuantity(quantity, fund, Number(price));
+                    buy(data);
+                    const alluserdata = await userModel.findOneAndUpdate(
+                        {
+                            _id: userData._id,
+                            isActive: true,
+                            isDelete: false,
+                            isVerified: true
+                        },
+                        {
+                            $inc: {
+                                totalUsePlan: +1
+                            }
+                        },
+                        { new: true }
+                    );
+                    console.log(alluserdata);
+                    return returnObj = {
+                        user_id: userData._id,
+                        tradingsymbol: body.tradingsymbol,
+                        buyOrderId: random5DigitNumber,
+                        quantity,
+                        isSelled: false,
+                        buyKitePrice: body.price,
+                        buyAT: indiaTime,
+                        accessToken: access_key,
+                        lessQuantity: false,
+                        buytradeStatus: null,
+                    };
+                }
+                else {
+                    const updatedQuantity = stockQuantity(quantity, fund, Number(price));
 
-        //             if (updatedQuantity > 0) {
+                    if (updatedQuantity > 0) {
 
-        //                 const random5DigitNumber = generateRandomNumber();
-        //                 const data: any = {
-        //                     access_key: userData.access_key,
-        //                     id: id,
-        //                     tradingsymbol: body.tradingsymbol,
-        //                     quantity: quantity,
-        //                     exchange: body.exchange,
-        //                     order_type: body.order_type,
-        //                     product: body.product
-        //                 };
+                        const random5DigitNumber = generateRandomNumber();
+                        const data: any = {
+                            access_key: userData.access_key,
+                            id: id,
+                            tradingsymbol: body.tradingsymbol,
+                            quantity: quantity,
+                            exchange: body.exchange,
+                            order_type: body.order_type,
+                            product: body.product
+                        };
 
-        //                 // buy(data);
-        //                 const alluserdata = await userModel.findOneAndUpdate(
-        //                     {
-        //                         _id: userData._id,
-        //                         isActive: true,
-        //                         isDelete: false,
-        //                         isVerified: true
-        //                     },
-        //                     {
-        //                         $inc: {
-        //                             totalUsePlan: +1
-        //                         }
-        //                     },
-        //                     { new: true }
-        //                 );
-        //                 console.log(alluserdata);
-        //                 return returnObj = {
-        //                     user_id: userData._id,
-        //                     tradingsymbol: body.tradingsymbol,
-        //                     buyOrderId: random5DigitNumber,
-        //                     quantity: updatedQuantity,
-        //                     isSelled: false,
-        //                     buyKitePrice: body.price,
-        //                     buyAT: indiaTime,
-        //                     accessToken: access_key,
-        //                     lessQuantity: true,
-        //                     buytradeStatus: null
-        //                 };
-        //             } else {
-        //                 return returnObj = {
-        //                     // tradeDate: indiaTime,
-        //                     user_id: id,
-        //                     trade_id: resultAdminTradeEnter._id,
-        //                     msg: "Insufficient balance",
-        //                     accessToken: access_key
-        //                 };
-        //             }
-        //         }
-        //     } else if (access_key === null) {
-        //         return returnObj = {
-        //             // tradeDate: indiaTime,
-        //             user_id: id,
-        //             trade_id: resultAdminTradeEnter._id,
-        //             msg: "user not login",
-        //         };
-        //     }
-        // }
-        // else {
-        //     return returnObj = {
-        //         // tradeDate: indiaTime,        
-        //         user_id: id,
-        //         trade_id: resultAdminTradeEnter._id,
-        //         msg: "user does not set quantity of trade",
-        //         accessToken: access_key
-        //     };
-        // }
+                        buy(data);
+                        const alluserdata = await userModel.findOneAndUpdate(
+                            {
+                                _id: userData._id,
+                                isActive: true,
+                                isDelete: false,
+                                isVerified: true
+                            },
+                            {
+                                $inc: {
+                                    totalUsePlan: +1
+                                }
+                            },
+                            { new: true }
+                        );
+                        console.log(alluserdata);
+                        return returnObj = {
+                            user_id: userData._id,
+                            tradingsymbol: body.tradingsymbol,
+                            buyOrderId: random5DigitNumber,
+                            quantity: updatedQuantity,
+                            isSelled: false,
+                            buyKitePrice: body.price,
+                            buyAT: indiaTime,
+                            accessToken: access_key,
+                            lessQuantity: true,
+                            buytradeStatus: null
+                        };
+                    } else {
+                        return returnObj = {
+                            // tradeDate: indiaTime,
+                            user_id: id,
+                            trade_id: resultAdminTradeEnter._id,
+                            msg: "Insufficient balance",
+                            accessToken: access_key
+                        };
+                    }
+                }
+            } else if (access_key === null) {
+                return returnObj = {
+                    // tradeDate: indiaTime,
+                    user_id: id,
+                    trade_id: resultAdminTradeEnter._id,
+                    msg: "user not login",
+                };
+            }
+        }
+        else {
+            return returnObj = {
+                // tradeDate: indiaTime,        
+                user_id: id,
+                trade_id: resultAdminTradeEnter._id,
+                msg: "user does not set quantity of trade",
+                accessToken: access_key
+            };
+        }
 
     } catch (error) {
         return res.status(500).json(new apiResponse(500, responseMessage.internalServerError, {}, error.message));
