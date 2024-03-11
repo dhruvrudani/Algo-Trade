a9a49994712b8aac1ba090bc35ef205ad1b22354
Harsh1782 <bhisraharsh1782@gmail.com>
2024-03-11 17:45:11 +0530

    Module : changes code structure to the aggregation and some changes

--- src/helpers/kiteConnect/index.ts
+++ src/helpers/kiteConnect/index.ts
@@ -1,7 +1,7 @@
 export * from "./kiteTradeAction";
 export * from "./buyTradeAndsellTrade";
 export * from "./userProfile";
-export * from "./tradeHistoryFunction";
+// export * from "./tradeHistoryFunction";
 export * from "./calculateUserOwnProfitAndLoss";
 export * from "./buyTradeAndsellTrade";
 export * from "./kiteTradeAction";
