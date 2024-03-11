435907a5cfb4a7274636cc650867602782c17844
mitdobariya6 <mitd.webito@gmail.com>
2024-03-11 12:05:44 +0530

    Model Helper : node OTPsender templet colour change

--- src/helpers/sendEmailHelper.ts
+++ src/helpers/sendEmailHelper.ts
@@ -12,6 +12,7 @@ export const sendEmailHelper = async (email, otp, subject) => {
 			pass: nodeMail.password,
 		},
 	});
+	console.log('object :>> ', nodeMail.mail,nodeMail.password);
 	let mailOptions = {
 		from: 'bhv1912@gmail.com',
 		to: email,
@@ -27,33 +28,33 @@ export const sendEmailHelper = async (email, otp, subject) => {
 		</head>
 		<body>
 			<div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2;height: calc(100% - 20px);justify-content: center;align-items: center;display: flex;justify-content: center;">
-				<div style="width:30%;padding:20px 0;background-color: #E1FAFF;box-shadow: 0 0.75rem 1.5rem
+				<div style="width:30%;padding:20px 0;background-color: #000000;box-shadow: 0 0.75rem 1.5rem
 				rgba(18,38,63,.03);padding: 24px;border-radius: 8px;position:
 				relative;">
 					<div style="text-align: center;">
 						<a href="#">
 							<img src="" alt="">
 						</a>
-						<h1 style="margin:0;text-transform: uppercase;letter-spacing: 1;font-weight: bold;color: #70A82C;">Patram kids</h1>
+						<h1 style="margin:0;text-transform: uppercase;letter-spacing: 1;font-weight: bold;color: #994d00;">Algo Trade</h1>
 						<div style="margin-top: 12px; text-align: center;">
-							<h3 style="text-transform: capitalize;margin-bottom: 0; color: #1E1E2D;">Here is your one time otp</h3>
-						<p style="margin-top: 0px; color:#1e1e2d;font-size: 15px;">to validate your email address !</p>
+							<h3 style="text-transform: capitalize;margin-bottom: 0; color: #f2f2f2;">Here is your one time otp</h3>
+						<p style="margin-top: 0px; color:#f2f2f2;font-size: 15px;">OTP expires in 3:00 min</p>
 						<div style="margin-top:38px;gap: 40px;/* text-align: center; */justify-content: center;position: relative;background-color: #fff;padding: px;align-items: center;font-size: 22px;border-radius: 10px;width: 100%;">
-						<h2 style="margin:0 0px 0 0px;color:#70a82c;padding:10px 0;letter-spacing: 18px;text-align: center;">${otp}
+						<h2 style="margin:0 0px 0 0px;color:#000000;padding:10px 0;letter-spacing: 18px;text-align: center;">${otp}
 						</h2>
 </div>
 <div style=" padding: 20px 0px 0;">
     <a href="http://localhost:3011/auth/change-password" style="
     text-decoration: none;
-    color: #1E1E2D;
+    color: #f2f2f2;
     font-size: 16px;
 ">Back to <span style="
     font-size: 16px;
-    color: #6CA83C;
+    color: #b35900;
     font-weight: 600;
     letter-spacing: 1px;
     text-transform: uppercase;
-">Patram kids</span></a>
+">Power By Webito </span></a>
 </div>
 						</div>
 					</div>
